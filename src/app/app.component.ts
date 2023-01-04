652e4ec31b19d23443f37528c249e68c0dd9ae30
philipcalugcugan <qwertyuiophilip@gmail.com>
2023-01-04 20:06:39 +0800

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,66 @@
-import { Component, VERSION } from '@angular/core';
-import { IisLocationService } from '@service/iis-location-service/iis-location-service';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { IisLocationData } from '../interface/iis-location-data';
+import { IisLocationService } from '../service/iis-location-service/iis-location.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  actionName: string;
+  isStarted: boolean;
+  timeInterval: number;
+  iisLocationList: IisLocationData[];
+  timer = null;
+
+  constructor(private iisLocationService: IisLocationService) {}
+
+  ngOnInit() {
+    this.actionName = 'Start';
+    this.isStarted = false;
+    this.timeInterval = 0;
+    this.iisLocationList = [];
+  }
+
+  public clickAction() {
+    if (this.isStarted) {
+      this.actionName = 'Start';
+      this.isStarted = false;
+      this.stopTimer();
+    } else {
+      this.actionName = 'Stop';
+      this.isStarted = true;
+
+      // Start timer
+      this.startTimer();
+    }
+  }
+
+  public startTimer() {
+    if (this.timeInterval > 0) {
+      this.timer = setInterval(() => {
+        this.iisLocationService.getIisLocation().subscribe((res) => {
+          if (res) {
+            console.log('timeInterval :' + this.timeInterval);
+            console.log(res);
+            let iisLocationData: IisLocationData = {
+              date: new Date().toISOString(),
+              altitude: res.altitude.toFixed(2),
+              velocity: res.velocity.toFixed(2),
+              daynum: res.daynum.toFixed(2),
+            };
+            this.iisLocationList.push(iisLocationData);
+          }
+        });
+      }, this.timeInterval * 1000);
+    }
+  }
+
+  public stopTimer() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+  }
 }
---
+++ src/interface/iis-location-data.ts
@@ -0,0 +1,13 @@
+export interface IisLocationData {
+  //name: string | undefined;
+  //id: number | undefined;
+  //latitude: number | undefined;
+  //longitude: number | undefined;
+  altitude: number | undefined;
+  velocity: number | undefined;
+  //visibility: number | undefined;
+  //footprint: number | undefined;
+  //timestamp: number | undefined;
+  daynum: number | undefined;
+  date: string | undefined;
+}
